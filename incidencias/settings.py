25d16914b2068eb816f59388e24e998bcce30ecf
Roldan Vargas <yguillen@ip-10-138-111-216.ec2.internal>
2015-05-12 14:21:02 +0000

    modificaciones en archivo de configuracion

--- incidencias/settings.py
+++ incidencias/settings.py
@@ -131,4 +131,4 @@ EMAIL_USE_TLS       = True
 EMAIL_FROM      = 'incidencias@bomberosmerida.gob.ve'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_COOKIE_AGE = 5 * 60
+SESSION_COOKIE_AGE = 3 * 60
